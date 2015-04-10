38d627058240b2d50669fed77fc42f02ac4eab6d
Daniel Helfrich <daniel.helfrich@colorado.edu>
2015-04-09 20:23:32 -0600

    All working well

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 CC=gcc
 CXX=g++
 RM=rm -f
-CPPFLAGS=-g -O2
+CPPFLAGS=-g -O2 -fopenmp
 LDFLAGS=-g 
 
 SRCS= main.cpp
 
 main: main.cpp main.h
-	$(OBJS) $(CXX) $(LDFLAGS) -o main $(OBJS) $(SRCS)
+	$(OBJS) $(CXX) $(CPPFLAGS) -o main $(OBJS) $(SRCS)
 
 clean:
 	rm -f $(FILES) *.o *~
--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 #include "main.h"
 
 
+//A, G, C, T are numbers between 1 and 10, summing to 10, representing
+//the proportion of bases in the random strand
+char randomBase(int A, int G, int C, int T)
+{
+  if(A + G + C + T != 10)
+    std::cout << "error: ATCG not in right form" << std::endl;
+  int ran = std::rand() % 10;
+  if(ran < A)
+    return 'A';
+  else if(ran < G + A)
+    return 'G';
+  else if(ran < C + G + A)
+    return 'C';
+  else if(ran < 10)
+    return 'T';
+  else{
+    std::cout << "error: couldn't pick base" << std::endl;
+    return 'E';
+  }
+}
+
 void createSeq(std::vector<char> &seq , int n)
 {
   for (int i = 0; i < n; ++i) //Create random sequence
-    seq.push_back(DNA[std::rand() % 4]);
+    seq.push_back(randomBase(1, 4, 2, 3));
 }
 
 //Brute force algorithm to find k,
@@ -14,8 +35,24 @@ void createSeq(std::vector<char> &seq , int n)
 int findk(const std::vector<char> & seq)
 {
   int n = seq.size();
-  for (int i = 1; i < n; ++i){ //for all k's less than the size
-
+  std::vector<char>::const_iterator first =seq.begin();
+  for (int k = 1; k < n; ++k){ //for all k's less than the size
+NEW_K: //Break out of loops
+    for (int i = 0; i < n - k; ++i){ //for each starting position
+      std::vector<char> kmer(first + i, first + i + k);
+      for (int j = i + 1; j < n; ++j){//check k-mer against remaining sequence
+        std::vector<char> section(first + j, first + j + k);
+        if(kmer == section){
+          //std::cout << "section at pos: " << i << " = section at pos: " << j << std::endl;
+          //std::cout << "section = ";
+          //std::copy(kmer.begin(), kmer.end(), std::ostream_iterator<char>(std::cout, " "));
+          //std::cout << std::endl;
+          k++;
+          goto NEW_K;
+        }
+      }
+    }
+    return k; //If here, then for each starting position, the kmer doesn't match any sequence
   }
 }
 
@@ -23,10 +60,30 @@ int findk(const std::vector<char> & seq)
 
 int main(int argc, char *argv[])
 {
-  std::srand(std::time(0));
-  std::vector<char> seq;
-  createSeq(seq, 10000);
-  for (std::vector<char>::iterator i = seq.begin();i  < seq.end(); i++)
-    std::cout << *i;
+  struct timespec ts;
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+  std::srand((time_t) ts.tv_nsec);
+  
+  int n = 10;
+  if (argc > 0)
+    n = atoi(argv[1]);
+
+  std::vector<char> seq;                                              
+  createSeq(seq, n);
+
+//  std::cout << "seq = "; 
+//  std::copy(seq.begin(), seq.end(), std::ostream_iterator<char>(std::cout, " "));
+//  std::cout << std::endl;
+  
+//  int countA = 0;
+//  for (std::vector<char>::const_iterator i = seq.begin(); i != seq.end(); ++i) {
+//    if (*i == 'C')
+//      countA++;
+//  }
+//  std::cout << "G appears " << countA << " times" << std::endl;
+  
+
+  int k = findk(seq);
+  std::cout << "k = " << k << std::endl;
   return 0;
 }
